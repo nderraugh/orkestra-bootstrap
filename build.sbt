7eca1255c0eaacb4c0298b76f9d4bec54093b9f8
Neil Derraugh <neil.derraugh@yoppworks.com>
2019-04-10 22:37:42 -0400

    initial commit

---
+++ build.sbt
@@ -0,0 +1,12 @@
+ThisBuild / dependencyOverrides += "org.webjars.npm" % "js-tokens" % "3.0.2"
+
+lazy val orkestra = orkestraProject("orkestra", file("orkestra"))
+  .settings(
+    libraryDependencies ++= Seq(
+      "tech.orkestra" %%% "orkestra-github" % orkestraVersion // Optional Github plugin
+      ,"tech.orkestra" %%% "orkestra-cron" % orkestraVersion // Optional Cron plugin
+      ,"tech.orkestra" %% "orkestra-lock" % orkestraVersion // Optional Lock plugin
+    )
+  )
+lazy val orkestraJVM = orkestra.jvm
+lazy val orkestraJS = orkestra.js
---
+++ orkestra/src/main/scala/orkestra.scala
@@ -0,0 +1,20 @@
+import tech.orkestra._
+import tech.orkestra.Dsl._
+import tech.orkestra.board._
+import tech.orkestra.job._
+import tech.orkestra.model._
+
+// We extend OrkestraServer to create the web server
+object Orkestra extends OrkestraServer {
+  // Configuring the UI
+  lazy val board = deployFrontendJobBoard
+  // Configuring the jobs
+  lazy val jobs = Set(deployFrontendJob)
+  
+  // Creating the job and configuring UI related settings
+  lazy val deployFrontendJobBoard = JobBoard[() => Unit](JobId("deployFrontend"), "Deploy Frontend")()
+  // Creating the job from the above definition (this will be compiled to JVM)
+  lazy val deployFrontendJob = Job(deployFrontendJobBoard) { implicit workDir => () =>
+    println("Deploying Frontend")
+  }
+}
